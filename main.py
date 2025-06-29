44101b50fc44d0ba3e0410fc1933a30ac86053b5
jeffschoe <ratacame@protonmail.com>
2025-06-29 17:04:13 -0400

    CH1 L4. Refined parsing of arguments.

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-import os
 import sys
+import os
 from google import genai
 from dotenv import load_dotenv
 
 
-
 def main():
     load_dotenv()
+
+    args = sys.argv[1:] # gather the words from the input, ignore the script name
+
+    if not args: # if no input was passed
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do i build a calulater app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args) # joins args into a string with spaces
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=sys.argv
+        contents=user_prompt
     )
-    if len(sys.argv) <= 1: # no argments provided
+    if len(sys.argv) <= 1: # no arguments provided
         print("Error: No arguments provided.")
         sys.exit(1)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
