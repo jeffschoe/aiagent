5f0b5c62f69558d5ae0feb67bec82e914d649ed8
jeffschoe <ratacame@protonmail.com>
2025-06-29 13:44:32 -0400

    CH1 L4. Allow user to enter their own input prompt. Program detects if no prompt was provided.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import os
+import sys
+from google import genai
 from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=sys.argv
+    )
+    if len(sys.argv) <= 1: # no argments provided
+        print("Error: No arguments provided.")
+        sys.exit(1)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if __name__ == "__main__":
+    main()
