9fe00fc4a9e11a0edb41cd8e8eee28d2af5ebdd2
jeffschoe <ratacame@protonmail.com>
2025-07-29 22:42:03 -0400

    CH1L5: added list for eventual messages and broke out content generation into its own function.

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import os
 import sys
-
-from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
 
 def main():
     load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    
-    if len(sys.argv) < 2: # no input prompt provided
-        print(f"Error: no prompt provided.")
-        sys.exit(1)
-    else:
-        prompt = " ".join(sys.argv[1:])
-    
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents= prompt
-    )
 
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents= messages,
+    )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
