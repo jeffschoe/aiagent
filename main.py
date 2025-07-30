b2c25ba1df23b50f0f04d472c4f1e5618bef26b2
jeffschoe <ratacame@protonmail.com>
2025-07-29 23:29:43 -0400

    CH1L6: cleaned up --verbose checking.

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     
-    args = sys.argv[1:]
-
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]: # [1:] to start at index 1, ignoring script name
+        if not arg.startswith("--"): # ignores flags
+            args.append(arg)
+                              
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
@@ -21,27 +25,22 @@ def main():
 
     user_prompt = " ".join(args)
 
-    # check for verbose arg, may want a new function
-    if "--verbose" in user_prompt:
-        verbose = True 
-        print(f"***DEBUG: VERBOSE TRUE")
-    else:
-        verbose = False
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose, user_prompt)
+    generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages, verbose, user_prompt):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents= messages,
     )
     if verbose:
-        print("User prompt:", user_prompt)
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
